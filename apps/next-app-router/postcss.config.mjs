7c2fe93ec8cf72a2cbea2455b7c59f80e70237f8
Yong Vuthivann <vuthivann.yong007@gmail.com>
2024-04-23 10:36:32 +0700

    feat(tailwindcss): add share tailwind config (#1)
    * feat(tailwind): add tailwind share tailwind config
    
    * feat: init new app router
    
    * feat(tailwind): add postcss and tailwind config in next app router
    
    * feat(config): remove tsconfig in root folder

---
+++ apps/next-app-router/postcss.config.mjs
@@ -0,0 +1,6 @@
+import baseConfig from "@repo/tailwindcss-config/postcss.config.mjs";
+
+/** @type {import('postcss-load-config').Config} */
+const config = { ...baseConfig };
+
+export default config;
